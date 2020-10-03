852730062921fdc3b2fcda83761b5018f0dbeb0d
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2020-10-03 22:51:00 +0100

    First Commit

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*#define EXIT_SUCCESS 0;*/
+
+int main(){
+	char buffer[127];
+	scanf("%s", buffer);
+	printf("Não quero saber\n");
+	return EXIT_SUCCESS;
+}
