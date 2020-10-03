258e63d5a6b60f5601ab60e2fd0bb13ad5ac2f2e
DiogoSantoss <diogosantoss360@hotmail.com>
2020-10-04 00:41:23 +0100

    update test.c

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*#define EXIT_SUCCESS 0;*/
 
@@ -11,12 +12,47 @@ void testedois(){
 	printf("socorro\n");
 }
 
+void SO(){
+	printf("SO pain level is: 1%%\n");
+}
+
+void explosion(){
+	char choice[127];
+	char bomb[2];
+
+	printf("Do you want to get a Segmentation fault error ?\nyes:1 no:2\n");
+	scanf("%s",choice);
+
+	switch(atoi(choice)){
+		case 1:
+			*(int*)0 = 0;
+			break;
+		
+		case 2:
+			printf("get trolled\n");
+			*(int*)0 = 0;
+			break;
+	}
+}
+
 int main(){
+
 	char buffer[127];
+
 	scanf("%s", buffer);
+
+	char* text = malloc(sizeof(char)*(strlen(buffer)+1));
+	strcpy(text,buffer);
+	printf("You said: %s\n",text);
+
 	printf("Não quero saber\n");
 
 	help();
+	testedois();
+	SO();
 
+	explosion();
+
+	free(text);
 	return EXIT_SUCCESS;
 }
