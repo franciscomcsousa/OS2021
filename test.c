d13aee7d2635633c0634bd4bc4a438d93249f27b
DiogoSantoss <diogosantoss360@hotmail.com>
2020-10-03 23:59:51 +0100

    funcionou?

--- test.c
+++ test.c
@@ -3,9 +3,16 @@
 
 /*#define EXIT_SUCCESS 0;*/
 
+void help(){
+	printf("what am I doing ?\n");
+}
+
 int main(){
 	char buffer[127];
 	scanf("%s", buffer);
 	printf("Não quero saber\n");
+
+	help();
+
 	return EXIT_SUCCESS;
 }
