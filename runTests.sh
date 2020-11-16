c01bf0d0db7c1b7fe75f4ab4a1f1748ceb422c89
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2020-11-16 22:54:22 +0000

    verified the existence of the runTests script input

--- runTests.sh
+++ runTests.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-inputdir=${1?Error: Invalid input directory}
-outputdir=${2}
-maxthreads=${3?Error: Invalid threads number}
+INPUTDIR=${1?Error: Invalid input directory}
+OUTPUTDIR=${2?Error: Invalid output directory}
+MAXTHREADS=${3?Error: Invalid threads number}
 
-mkdir ${outputdir}
+if [ ! -d "$INPUTDIR" ]
+then
+    echo "Error: $INPUTDIR is not a valid directory"
 
-for filepath in $inputdir/*.txt
-do
-    filename=$(basename $filepath) 
-    for i in $(seq 1 ${maxthreads})
-    do                                                               
-        echo InputFile="${filename//.txt}" NumThreads="$i"                                            
-        outputfilename=${filename//.txt}-$i                                                 
-        ./tecnicofs $filepath ${outputdir}/${outputfilename}.txt $i | grep "TecnicoFS" 
-    done     
-done
+elif [ -e "$OUTPUTDIR" ]
+then
+    echo "Error: $OUTPUTDIR already exists"
+
+elif [ "$MAXTHREADS" -lt 0 ]
+then
+    echo "Error: $MAXTHREADS is not a valid number of threads"
+
+else
+    mkdir ${OUTPUTDIR}
+    for FILEPATH in $INPUTDIR/*.txt
+    do
+        FILENAME=$(basename $FILEPATH) 
+        for i in $(seq 1 ${MAXTHREADS})
+        do                                                               
+            echo InputFile="${FILENAME//.txt}" NumThreads="$i"                                            
+            OUTPUTFILENAME=${FILENAME//.txt}-$i                                                 
+            ./tecnicofs $FILEPATH ${OUTPUTDIR}/${OUTPUTFILENAME}.txt $i | grep "TecnicoFS" 
+        done     
+    done
+fi
